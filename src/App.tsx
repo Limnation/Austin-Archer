f073d81c07a8c3b3fdd7cc9b3b030b6c34a8be91
Scott Erwin <scottallenerwin@gmail.com>
2022-11-28 23:12:27 -0500

    removed margin on body

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Header } from './lib/Header/header'
 import { Button } from './lib/Button/button'
+import { Section } from './lib/Section/section';
 
 const App = () => {
   return (
-    <>
+    <Section>
       <Header />
       <Button />
-    </>
+    </Section>
   );
 }
 
--- src/lib/Section/section.styled.tsx
+++ src/lib/Section/section.styled.tsx
@@ -9,8 +9,8 @@ const StyledSection = styled.section<SectionInterface>`
     color: #e113da;
     background-color: ${props => props.theme.color || "palevioletred"};
     background-image: ${props => props.theme.backgroundImage || "url(/img/image.png)" };
-    width: ${ props => props.theme.width || "1vw" };
-    height: ${ props => props.theme.height || "1vh" };
+    width: ${ props => props.theme.width || "100vw" };
+    height: ${ props => props.theme.height || "100vh" };
 `;
 
 export { StyledSection };
