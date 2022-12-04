7a650dfdac3599aca7ffab74667c48bd48277b1d
Scott Erwin <scottallenerwin@gmail.com>
2022-12-04 13:39:46 -0500

    updated boxes interface

--- src/lib/Box/box.styled.tsx
+++ src/lib/Box/box.styled.tsx
@@ -1,14 +1,23 @@
+import { number, string } from 'prop-types';
 import styled from 'styled-components';
 
 interface BoxInterface { 
+    width?: string;
+    height?: string;
     color?: string;
-    background?: string;
+    margin?: string | number;
+    padding?: string | number;
+    backgroundColor?: string;
+    backgroundImage?: any;
+    backgroundSize?: string;
 };
 
 const StyledBox = styled.div<BoxInterface>`
     width: ${ props => props.theme.width || "100vw" };
     height: ${ props => props.theme.height || "100vh" };
-    color: ${props => props.theme.color || "#ffffff"};
+    color: ${props => props.theme.color};
+    margin: ${props => props.theme.margin};
+    padding: ${props => props.theme.padding};
     background-color: ${props => props.theme.backgroundColor};
     background-image: ${props => props.theme.backgroundImage};
     background-size: ${ props => props.theme.backgroundSize};
